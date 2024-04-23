0948496cef8402c3ae07d62da092f51885681d8b
Rohan Parmar <rohanrparmar987@gmail.com>
2024-04-24 00:34:52 +0530

    GET book by ID

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,21 @@ app.post('/book', async (request, response) => {
 app.get('/books', async (request, response) => {
   try {
     const books = await Book.find({});
-    return response.status(200).json(books);
+    return response.status(200).json({
+      count: books.length,
+      data: books
+    });
+  } catch (error) {
+    return response.status(500).send({ message: error.message });
+  }
+})
+
+app.get('/books/:id', async (request, response) => {
+  try {
+
+    const { id } = request.params;
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
   } catch (error) {
     return response.status(500).send({ message: error.message });
   }
