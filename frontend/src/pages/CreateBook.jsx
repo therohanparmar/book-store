2efc1996416923ceee7b0f8bc971741c2b4f8ff9
Rohan Parmar <rohanrparmar987@gmail.com>
2024-04-25 23:26:39 +0530

    Configure Routes

---
+++ frontend/src/pages/CreateBook.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CreateBook = () => {
+  return (
+    <div>CreateBook</div>
+  )
+}
---
+++ frontend/src/pages/DeleteBook.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DeleteBook = () => {
+  return (
+    <div>DeleteBook</div>
+  )
+}
